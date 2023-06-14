37ac4c3f48f2760787be44d1036cd0d1aa744a52
Ilia Grigorev <ilia_grigorev@mail.ru>
2023-06-14 14:19:15 +0200

    final edits

--- uni_plaza/users/views.py
+++ uni_plaza/users/views.py
@@ -57,3 +57,11 @@ def profile(request):
         form = UserProfileForm(instance=request.user)
     context = {'title': 'Профиль', 'form': form}
     return render(request, 'users/profile.html', context)
+
+
+"""Выход из профиля"""
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
