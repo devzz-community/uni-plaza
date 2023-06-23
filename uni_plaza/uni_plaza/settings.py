d36db638251002849f323ba320435315a8c8db2f
Ilia Grigorev <ilia_grigorev@mail.ru>
2023-06-23 18:54:36 +0200

    edits shop_views and users_views

--- uni_plaza/uni_plaza/settings.py
+++ uni_plaza/uni_plaza/settings.py
@@ -137,6 +137,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
 LOGIN_URL = '/users/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 # Import Export
 
--- uni_plaza/users/views.py
+++ uni_plaza/users/views.py
@@ -1,73 +1,46 @@
-from django.shortcuts import render, HttpResponseRedirect
-from django.contrib import auth, messages
-from django.urls import reverse
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView, UpdateView
+from django.contrib.auth.views import LoginView
+from django.contrib.messages.views import SuccessMessageMixin
 from users.models import User
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 from shop.models import Basket
-from django.contrib.auth.decorators import login_required
+from common.views import TitleMixin
 
 """ Вход пользователя """
 
 
-def login(request):
-    if request.method == 'POST':
-        form = UserLoginForm(data=request.POST)
-        if form.is_valid():
-            username = request.POST['username']
-            password = request.POST['password']
-            user = auth.authenticate(username=username, password=password)
-            if user:
-                auth.login(request, user)
-                return HttpResponseRedirect(reverse('index'))
-    else:
-        form = UserLoginForm()
-    context = {'form': form}
-    return render(request, 'users/login.html', context)
+class UserLoginView(TitleMixin, LoginView):
+    template_name = 'users/login.html'
+    form_class = UserLoginForm
+    title = 'Авторизация'
 
 
 """Регистрация пользователя"""
 
 
-def registration(request):
-    if request.method == 'POST':
-        form = UserRegistrationForm(data=request.POST)
-        print()
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Вы успешно зарегистрированы!')
-            return HttpResponseRedirect(reverse('users:login'))
-        else:
-            print(form.errors)
-    else:
-        form = UserRegistrationForm()
-    context = {'form': form}
-    return render(request, 'users/registration.html', context)
+class UserRegistrationView(TitleMixin, SuccessMessageMixin, CreateView):
+    model = User
+    form_class = UserRegistrationForm
+    template_name = 'users/registration.html'
+    title = 'Регистрация'
+    success_url = reverse_lazy('users:login')
+    success_message = 'Вы успешно зарегистрированы!'
 
 
 """Личный кабинет пользователя"""
 
 
-@login_required
-def profile(request):
-    if request.method == 'POST':
-        form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('users:profile'))
-        else:
-            print(form.errors)
-    else:
-        form = UserProfileForm(instance=request.user)
-    context = {'title': 'Профиль',
-               'form': form,
-               'baskets': Basket.objects.filter(user=request.user)
-               }
-    return render(request, 'users/profile.html', context)
+class UserProfileView(TitleMixin, UpdateView):
+    model = User
+    form_class = UserProfileForm
+    template_name = 'users/profile.html'
+    title = 'Личный кабинет'
 
+    def get_success_url(self):
+        return reverse_lazy('users:profile', args=(self.object.id,))
 
-"""Выход из профиля"""
-
-
-def logout(request):
-    auth.logout(request)
-    return HttpResponseRedirect(reverse('index'))
+    def get_context_data(self, **kwargs):
+        context = super(UserProfileView, self).get_context_data()
+        context['baskets'] = Basket.objects.filter(user=self.object)
+        return context
